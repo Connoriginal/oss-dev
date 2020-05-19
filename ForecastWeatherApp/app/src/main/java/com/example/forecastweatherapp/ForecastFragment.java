529e2f9405788c45799956d0998c8f7ecb8c75b1
권태윤 <kwontaeyoon@gwontaeyun-ui-MacBookPro.local>
2020-05-19 22:41:51 +0900

    assingment 8/10 Use Service

--- ForecastWeatherApp/app/src/main/java/com/example/forecastweatherapp/ForecastFragment.java
+++ ForecastWeatherApp/app/src/main/java/com/example/forecastweatherapp/ForecastFragment.java
@@ -2,16 +2,19 @@ package com.example.forecastweatherapp;
 
 
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 
 
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.preference.PreferenceManager;
 import android.text.format.Time;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -22,20 +25,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -45,7 +36,49 @@ import java.util.List;
 public class ForecastFragment extends Fragment {
 
     private ArrayAdapter<String> forecastAdapter;
+    private IFetchWeatherService mService;
 
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(FetchWeatherService.ACTION_RETRIEVE_WEATHER_DATA)) {
+                String[] data = intent.getStringArrayExtra(FetchWeatherService.EXTRA_WEATHER_DATA);
+                forecastAdapter.clear();
+                for(String dayForecastStr : data) {
+                    forecastAdapter.add(dayForecastStr);
+                }
+            }
+        }
+    };
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mService = IFetchWeatherService.Stub.asInterface(service);
+
+            try {
+                mService.registerFetchDataListener(mFetchDataListener);
+            } catch (RemoteException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+        }
+    };
+
+    private IFetchDataListener.Stub mFetchDataListener = new IFetchDataListener.Stub() {
+        @Override
+        public void onWeatherDataRetrieved(String[] data) throws RemoteException {
+            forecastAdapter.clear();
+            for(String dayForecastStr : data) {
+                forecastAdapter.add(dayForecastStr);
+            }
+        }
+    } ;
 
     public ForecastFragment(){
 
@@ -55,6 +88,21 @@ public class ForecastFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
+        getActivity().bindService(new Intent(getActivity(), FetchWeatherService.class), mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mService != null) {
+            try {
+                mService.unregisterFetchDataListener(mFetchDataListener);
+            } catch (RemoteException ex) {
+                ex.printStackTrace();
+            }
+        }
+        getActivity().unbindService(mServiceConnection);
+        super.onDestroy();
     }
 
     @Override
@@ -74,10 +122,16 @@ public class ForecastFragment extends Fragment {
     }
 
     private void refreshWeatherData(){
-        FetchWeatherTask weatherTask = new FetchWeatherTask();
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        String cityId = prefs.getString("city", "1835847");
-        weatherTask.execute(cityId);
+//        Intent intent = new Intent(getActivity(), FetchWeatherService.class);
+//        intent.setAction(FetchWeatherService.ACTION_RETRIEVE_WEATHER_DATA);
+//        getActivity().startService(intent);
+        if (mService != null) {
+            try {
+                mService.retrieveWeatherData();
+            } catch (RemoteException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     @Nullable
@@ -128,169 +182,10 @@ public class ForecastFragment extends Fragment {
         return rootView;
     }
 
-    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
-
-        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-
-        private String getDate(long time){
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            return simpleDateFormat.format(time);
-        }
-
-        private String formatHighLows(double high, double low) {
-            // For presentation, assume the user doesn't care about tenths of a degree.
-            long roundedHigh = Math.round(high);
-            long roundedLow = Math.round(low);
-
-            String highLowStr = roundedHigh + "/" + roundedLow;
-            return highLowStr;
-        }
-
-
-        private String[] getWeatherDataFromJson(String forecastJsonStr,int numDays)
-                throws JSONException {
-            //JSON objects
-            final String OWM_LIST = "list";
-            final String OWM_TEMPERATURE = "temp";
-            final String OWM_WEATHER = "weather";
-
-            JSONObject forecastJson = new JSONObject(forecastJsonStr);
-            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
-
-            Time dayTime = new Time();
-            dayTime.setToNow();
-
-            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
-
-            dayTime = new Time();
-
-            String[] resultStrs = new String[numDays];
-            for(int i = 0 ; i < weatherArray.length(); i++){
-                String day;
-                String tempHighAndLow;
-                String description;
-
-                JSONObject dayForecast = weatherArray.getJSONObject(i);
-
-                long dateTime = dayTime.setJulianDay(julianStartDay+i);
-                day = getDate(dateTime);
-
-                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
-                double high = temperatureObject.getDouble("max");
-                double low = temperatureObject.getDouble("min");
-                tempHighAndLow = formatHighLows(high,low);
-
-                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
-                description = weatherObject.getString("main");
-
-                resultStrs[i] = day + ",\nWeather: " + description + " and the temperature (high/low) :" + tempHighAndLow;
-            }
-
-            for(String s : resultStrs){
-                Log.v(LOG_TAG, "Forecast entry: " + s);
-            }
-            return resultStrs;
-        }
-        @Override
-        protected String[] doInBackground(String... strings) {
-
-            if(strings.length == 0) return null;
-
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-
-            String forecastJsonStr = null;
-
-
-            String format = "json";
-            String units = "metric";
-            int numDays = 7;
-            try {
-
-                final String FORECAST_BASE_URL =
-                        "https://api.openweathermap.org/data/2.5/forecast/daily?";
-                final String QUERY_PARAM = "id";
-                final String FORMAT_PARAM = "mode";
-                final String UNITS_PARAM = "units";
-                final String DAYS_PARAM = "cnt";
-                final String APPID_PARAM = "APPID";
-
-                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                        .appendQueryParameter(QUERY_PARAM, strings[0])
-                        .appendQueryParameter(FORMAT_PARAM, format)
-                        .appendQueryParameter(UNITS_PARAM, units)
-                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-                        .appendQueryParameter(APPID_PARAM, "5fd2f2cde90c1533efb95b19c048a528")
-                        .build();
-
-                URL url = new URL(builtUri.toString());
-
-                Log.v(LOG_TAG, "Built URI " + builtUri.toString());
-
-                urlConnection = (HttpURLConnection) url.openConnection();
-                Log.v(LOG_TAG,"여기는왔냐?ㅅ");
-                urlConnection.setRequestMethod("GET");
-                Log.v(LOG_TAG,"여기는왔냐?ㅅㅂㅅㅃㅅ");
-                Log.v(LOG_TAG,urlConnection.toString());
-
-
-                urlConnection.connect();
-                Log.v(LOG_TAG,"여기는왔냐?여기가 문제냐?");
-
-                Log.v(LOG_TAG,"여기는왔냐?");
-                InputStream inputStream = urlConnection.getInputStream();
-                Log.v(LOG_TAG,"여기는왔냐?너냐?");
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) return null;
-                Log.v(LOG_TAG,"여기는왔냐?2");
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                Log.v(LOG_TAG,"여기는왔냐?3");
-                while ((line = reader.readLine()) != null) buffer.append(line + "\n");
-
-                if (buffer.length() == 0) return null;
-
-                forecastJsonStr = buffer.toString();
-
-                Log.v(LOG_TAG, "Forecast string: " + forecastJsonStr);
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-
-            try {
-                return getWeatherDataFromJson(forecastJsonStr, numDays);
-            } catch (JSONException e) {
-                Log.e(LOG_TAG, e.getMessage(), e);
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String[] result) {
-            if (result != null) {
-                forecastAdapter.clear();
-                for(String dayForecastStr : result) {
-                    forecastAdapter.add(dayForecastStr);
-                }
-            }
-        }
+    @Override
+    public void onDestroyView() {
+        //getActivity().unregisterReceiver(mBroadcastReceiver);
+        super.onDestroyView();
     }
-
 }
 
--- ForecastWeatherApp/app/src/main/java/com/example/forecastweatherapp/SettingsActivity.java
+++ ForecastWeatherApp/app/src/main/java/com/example/forecastweatherapp/SettingsActivity.java
@@ -19,9 +19,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         bindPreferenceSummaryToValue(findPreference("city"));
     }
 
-    /**
-     * Set up the {@link android.app.ActionBar}, if the API is available.
-     */
     private void setupActionBar() {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -42,30 +39,23 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         return super.onMenuItemSelected(featureId, item);
     }
 
-    /**
-     * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
-     */
+
     private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
         @Override
         public boolean onPreferenceChange(Preference preference, Object value) {
             String stringValue = value.toString();
 
             if (preference instanceof ListPreference) {
-                // For list preferences, look up the correct display value in
-                // the preference's 'entries' list.
+
                 ListPreference listPreference = (ListPreference) preference;
                 int index = listPreference.findIndexOfValue(stringValue);
 
-                // Set the summary to reflect the new value.
                 preference.setSummary(
                         index >= 0
                                 ? listPreference.getEntries()[index]
                                 : null);
 
             } else {
-                // For all other preferences, set the summary to the value's
-                // simple string representation.
                 preference.setSummary(stringValue);
             }
             return true;
@@ -73,11 +63,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     };
 
     private static void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
         preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
 
-        // Trigger the listener immediately with the preference's
-        // current value.
         sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
                 PreferenceManager
                         .getDefaultSharedPreferences(preference.getContext())
